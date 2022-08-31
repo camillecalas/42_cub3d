1c7187d4e5f69d19c8cb64229dbf8ddcc861c19c
Camille CALAS <ccalas@e2r4p5.clusters.42paris.fr>
2022-08-31 18:12:57 +0200

    map check

--- srcs/map.c
+++ srcs/map.c
@@ -1,8 +1,127 @@
 #include "../include/cub3D.h"
 
+void	ft_check_direction(t_init *init)
+{
+	int	nswe;
+	int	i;
+	int	j;
+
+	nswe = 0;
+	i = 0;
+	while (init->map[i])
+	{	
+		j = 0;
+		while (init->map[i][j])
+		{
+			if (init->map[i][j] == 'S' || init->map[i][j]  == 'N'
+				|| init->map[i][j]  == 'W' || init->map[i][j]  == 'E')
+				nswe++;
+			j++;
+		}
+		i++;
+	}
+	if (nswe != 1)
+	{
+		ft_putstr_fd("Error\nInvalid character direction\n", 2);
+		// A FREE
+		exit(1);
+	}
+}
+
+void	ft_check_authorised_char(t_init *init)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (init->map[i])
+	{	
+		j = 0;
+		while (init->map[i][j])
+		{
+			if (init->map[i][j] != '1' && init->map[i][j] != '0'
+				&& init->map[i][j] != ' ' && init->map[i][j] != '\n'
+				&& init->map[i][j] != 'S' && init->map[i][j]  != 'N'
+				&& init->map[i][j] != 'W' && init->map[i][j]  != 'E')
+			{
+				ft_putstr_fd("Error\nInvalid character\n", 2);
+				// A FREE
+				exit(1);
+			}
+			j++;
+		}
+		i++;
+	}
+}
+
+void	ft_check_close_map(t_init *init)
+{
+	int	i;
+	int	j;
+	int	i2;
+	int	j2;
+
+	i = 0;
+	while (init->map[i])
+	{	
+		j = 0;
+		while (init->map[i][j])
+		{
+			if (init->map[i][j] == '0' || init->map[i][j] == 'S' || init->map[i][j]  == 'N'
+				|| init->map[i][j]  == 'W' || init->map[i][j]  == 'E')
+			{
+				i2 = i;
+				while (i2 >= 0 && init->map[i2][j] != '1' && init->map[i2][j] != ' ')
+					i2--;
+				if (i2 < 0 || init->map[i2][j] == ' ')
+				{
+					printf("i: %d", i2);
+					printf("j: %d", j);
+					ft_putstr_fd("Error\nMap not closed\n", 2);
+					exit(0);
+				}
+				i2 = i;
+				while (init->map[i2][j] && init->map[i2][j] != '1' && init->map[i2][j] != ' ')
+					i2++;
+				if (!init->map[i2] || init->map[i2][j] == ' ')
+				{
+					printf("i: %d", i2);
+					printf("j: %d", j);
+					ft_putstr_fd("Error\nMap not closed\n", 2);
+					exit(0);
+				}
+				j2 = j;
+				while (j2 >= 0 && init->map[i][j2] != '1' && init->map[i][j2] != ' ')
+					j2--;
+				if (j2 < 0 || init->map[i][j2] == ' ')
+				{
+					printf("i: %d", i);
+					printf("j: %d", j2);
+					ft_putstr_fd("Error\nMap not closed\n", 2);
+					exit(0);
+				}
+				j2 = j;
+				while (init->map[i][j2] && init->map[i][j2] != '1' && init->map[i][j2] != ' ')
+					j2++;
+				if ((size_t)j2 == ft_strlen(init->map[i]) || init->map[i][j2] == ' ')
+				{
+					printf("i: %d", i);
+					printf("j: %d", j2);
+					ft_putstr_fd("Error\nMap not closed\n", 2);
+					exit(0);
+				}
+			}
+			j++;
+		}
+		i++;
+	}
+}
+
 void	ft_check_map(t_init *init)
 {
-
+	ft_check_direction(init);
+	ft_check_authorised_char(init);
+	ft_check_close_map(init);
 }
 
 void	ft_cpy_map(int fd, t_init *init, char *filepath, int n)
