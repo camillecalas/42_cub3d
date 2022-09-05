2167011d759cf579d63e340709427fdce7e73634
Benjamin AUBIGNAT <baubigna@e2r7p12.clusters.42paris.fr>
2022-09-05 15:10:37 +0200

    working on inversed y-coord

--- srcs/loop.c
+++ srcs/loop.c
@@ -5,35 +5,40 @@ void	ft_step_and_sidedist(t_init *init, double *stepX, double *stepY)
 	if (init->game->rayDirX < 0)
 	{
 		*stepX = -1;
-		init->game->sideDistX = (init->game->posX - init->game->mapX)
-			* init->game->deltaDistX;
+		// init->game->sideDistX = (init->game->posX - init->game->mapX)
+		// 	* init->game->deltaDistX;
 	}
-	else
+	else if (init->game->rayDirX > 0)
 	{
 		*stepX = 1;
-		init->game->sideDistX = (init->game->mapX + 1 - init->game->posX)
-			* init->game->deltaDistX;
+		// init->game->sideDistX = (init->game->mapX + 1 - init->game->posX)
+			// * init->game->deltaDistX;
 	}
+	else
+		*stepX = 0;
 	if (init->game->rayDirY < 0)
 	{
 		*stepY = -1;
-		init->game->sideDistY = (init->game->posY - init->game->mapY)
-			* init->game->deltaDistY;
+		// init->game->sideDistY = (init->game->posY - init->game->mapY)
+		// 	* init->game->deltaDistY;
 	}
-	else
+	else if (init->game->rayDirY > 0)
 	{
 		*stepY = 1;
-		init->game->sideDistY = (init->game->mapY + 1 - init->game->posY)
-			* init->game->deltaDistY;
+		// init->game->sideDistY = (init->game->mapY + 1 - init->game->posY)
+		// 	* init->game->deltaDistY;
 	}
+	else
+		*stepY = 0;
 }
 
 void	ft_dda(t_init *init, int *side, double stepX, double stepY)
 {
 	int	hit;
+	int	x;
+	int	y;
 
 	hit = 0;
-	// dans cette boucle doit etre inclus une mise a jour de la valeur de deltaDist
 	while (!hit)
 	{
 		if (init->game->sideDistX < init->game->sideDistY)
@@ -48,10 +53,17 @@ void	ft_dda(t_init *init, int *side, double stepX, double stepY)
 			init->game->mapY += stepY;
 			*side = 1;
 		}
-		hit = 1;
+		printf("mapX: %f\n", init->game->mapX);
+		printf("mapY: %f\n", init->game->mapY);
 		// if (init->map[(int)init->game->mapX][(int)init->game->mapY] == '1')
 		// 	hit++;
+		x = (int)init->game->mapX;
+		y = (int)init->game->mapY;
+		printf("map ici: %c\n", init->map[13][20]);
+		// printf("map ici: %c\n", init->map[y][x]);
 	}
+	printf("mapX: %f\n", init->game->mapX);
+	printf("mapY: %f\n", init->game->mapY);
 }
 
 void	ft_game_loop(t_init *init)
@@ -63,7 +75,7 @@ void	ft_game_loop(t_init *init)
 
 	while (1)
 	{
-		i = 960;
+		i = 480;
 		while (i < WIDTH)
 		{
 			init->game->cameraX = 2 * i / (double)WIDTH - 1;
@@ -73,22 +85,29 @@ void	ft_game_loop(t_init *init)
 				* init->game->cameraX;
 			init->game->mapX = init->game->posX;
 			init->game->mapY = init->game->posY;
+			printf("mapX: %f\n", init->game->mapX);
+			printf("mapY: %f\n", init->game->mapY);
 			printf("rayDirX: %f\n", init->game->rayDirX);
 			printf("rayDirY: %f\n", init->game->rayDirY);
 			if (!init->game->rayDirX)
-				init->game->deltaDistX = 1e30;
+				init->game->deltaDistX = 0;
 			else
-				init->game->deltaDistX = fabs(1 / init->game->rayDirX);
+				init->game->deltaDistX = fabs(sqrt(pow(init->game->rayDirX, 2) + pow(init->game->rayDirY, 2)) / init->game->rayDirX);
 			printf("deltaDistX: %f\n", init->game->deltaDistX);
 			if (!init->game->rayDirY)
-				init->game->deltaDistY = 1e30;
+				init->game->deltaDistY = 0;
 			else
-				init->game->deltaDistY = fabs(1 / init->game->rayDirY);
+				init->game->deltaDistY = fabs(sqrt(pow(init->game->rayDirX, 2) + pow(init->game->rayDirY, 2)) / init->game->rayDirY);
 			printf("deltaDistY: %f\n", init->game->deltaDistY);
+			init->game->sideDistX = init->game->deltaDistX;
+			init->game->sideDistY = init->game->deltaDistY;
 			ft_step_and_sidedist(init, &stepx, &stepy);
 			printf("sideDistX: %f\n", init->game->sideDistX);
 			printf("sideDistY: %f\n", init->game->sideDistY);
+			printf("stepX: %f\n", stepx);
+			printf("stepY: %f\n", stepy);
 			ft_dda(init, &side, stepx, stepy);
+			printf("side: %d\n", side);
 			if (!side)
 				init->game->perpWallDist = init->game->sideDistX
 					- init->game->deltaDistX;
