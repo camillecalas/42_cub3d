64b9f8dfee9d109638005c2f62bac3437b59d74d
Benjamin AUBIGNAT <baubigna@e2r8p6.clusters.42paris.fr>
2022-09-21 14:22:06 +0200

    changed leaks

--- srcs/textures.c
+++ srcs/textures.c
@@ -6,7 +6,7 @@
 /*   By: baubigna <baubigna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/21 11:12:54 by baubigna          #+#    #+#             */
-/*   Updated: 2022/09/21 13:29:17 by baubigna         ###   ########.fr       */
+/*   Updated: 2022/09/21 14:06:06 by baubigna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,10 +43,7 @@ void	ft_check_map_line(t_init *init, int i, int j)
 	{
 		while (i2 >= 0 && init->map[i2] && init->map[i2][j]
 			&& (init->map[i2][j] == '0' || ft_is_player(init->map[i2][j])))
-			{
-				printf("%c", init->map[i2][j]);
 			i2--;
-			}
 		if (i2 < 0 || (init->map[i2] && !init->map[i2][j])
 			|| (init->map[i2] && init->map[i2][j] && init->map[i2][j] != '1'))
 			ft_error("Error\nMap not closed\n", init);
